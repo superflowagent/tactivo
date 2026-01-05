14b574ec83644a399130c75e00b75ce9263602a6
superflowagent <superflow.agent@gmail.com>
2026-01-05 01:50:37 +0100

    chore: remove all migration files (permanent)

--- supabase/migrations/025_grant_profiles_user_company_select.sql
+++
@@ -1,7 +0,0 @@
--- Migration: Allow SELECT on minimal columns (user, company) from profiles for authenticated role
-
-BEGIN;
-
-GRANT SELECT ("user", company) ON public.profiles TO authenticated;
-
-COMMIT;
