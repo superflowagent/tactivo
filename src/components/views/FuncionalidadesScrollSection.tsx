94ce8e13893a2f1c9537d307a24689ee6e3edb3d
superflowagent <superflow.agent@gmail.com>
2026-01-13 13:15:42 +0100

    chore(landing): delete FuncionalidadesScrollSection component file

--- src/components/views/FuncionalidadesScrollSection.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export default function FuncionalidadesScrollSection() {
-  // Sección eliminada: stub para evitar imports rotos si quedara alguna referencia.
-  return null;
-}
