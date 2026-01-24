a741efa7f54f30bdd0b49fa8eebdb7b6d1b16128
superflowagent <superflow.agent@gmail.com>
2026-01-24 18:42:34 +0100

    chore: remove unused scripts and files

--- scripts/download-supabase-functions.ps1
+++
@@ -1,5 +0,0 @@
-$funcs = supabase functions list --project-ref hzztmtkdaofrwcwzotas --output json | ConvertFrom-Json
-foreach ($f in $funcs) {
-  Write-Host "Downloading: $($f.name)"
-  supabase functions download $f.name --project-ref hzztmtkdaofrwcwzotas
-}
